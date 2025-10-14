05ba2f4c44d2cf3586c035148e64184e8b2464c4
ChenCMD <scnmed.g@gmail.com>
2025-10-15 04:10:41 +0900

    :zap: 各種ページ読み込みを非同期を遅延させ、ローディング表示を有効化するように

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import { Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from '@/components/theme-provider'
-import Home from '@/pages/Home'
-import Creator from '@/pages/Creator'
-import Download from '@/pages/Download'
-import FAQ from '@/pages/FAQ'
-import License from '@/pages/License'
+import { Suspense } from 'react'
+import lazyImport from './lib/lazyImport'
+import LoadingOverlay from '@/components/LoadingOverlay'
+
+const Home = lazyImport(() => import('@/pages/Home'))
+const Creator = lazyImport(() => import('@/pages/Creator'))
+const Download = lazyImport(() => import('@/pages/Download'))
+const FAQ = lazyImport(() => import('@/pages/FAQ'))
+const License = lazyImport(() => import('@/pages/License'))
 
 function App() {
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
       <div className="min-h-screen bg-background">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/creator" element={<Creator />} />
-          <Route path="/download" element={<Download />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/license" element={<License />} />
-        </Routes>
+        <Suspense fallback={<LoadingOverlay />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/creator" element={<Creator />} />
+            <Route path="/download" element={<Download />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/license" element={<License />} />
+          </Routes>
+        </Suspense>
       </div>
     </ThemeProvider>
   )
 }
 
-export default App 
+export default App
